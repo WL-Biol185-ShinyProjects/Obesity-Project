1bba637abc46ef36169fe6b09506e7a5751d349e
smithl20 <smithl20@mail.wlu.edu>
2019-10-29 12:18:01 -0400

    made comments on ui.r about outline of site and also filtered data only for 2018

--- ui.r
+++ ui.r
@@ -1,2 +1,15 @@
 library(shiny)
 View(obesityData)
+
+
+#Make header page
+  #on headed page need heat map of % of adults 15+ with obesity
+  #add desciption of why this is relevant
+
+#2nd tab
+  #Education- another heat map with % of education level on each one as a pin
+  #box plot below of x axis=state and y= % obese, color code with education levels (drop down box for state and check box box for education level)
+
+#3rd tab
+  #Poverty level with same obesity heat map
+  #bar graph x= income level (check box) and y=% obese, color by state (maybe drop down) 
