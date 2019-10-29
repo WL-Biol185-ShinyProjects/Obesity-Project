1f8ce96d73fcc57c9b7e73c6fb72fefd92be18b1
brophyk20 <brophyk20@mail.wlu.edu>
2019-10-29 13:08:16 -0400

    initial structure

---
+++ obesityEducation.r
@@ -0,0 +1,43 @@
+tabItem(
+  title = "",
+  titlePanel(""),
+  
+  sidebarLayout(
+    sidebarPanel(
+      checkboxInput(
+        inputId = "",
+        label = "",
+        value = TRUE
+      ),
+      checkboxInput(
+        inputId = "",
+        label = "",
+        value = TRUE
+      ),    
+      checkboxInput(
+        inputId = "",
+        label = "",
+        value = TRUE
+      ),     
+      checkboxInput(
+        inputId = "",
+        label = "",
+        value = TRUE
+      ),     
+      checkboxInput(
+        inputId = "",
+        label = "",
+        value = TRUE
+      )),
+      sliderInput("",
+                  "",
+                  min = ,
+                  max = ,
+                  value = c())
+    ),
+    
+    
+    mainPanel(
+      plotOutput("distPlotCarat")
+)
+)
