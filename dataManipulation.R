2b2b8e7351aaf0915d27e932ba5f65bb868ff99b
smithl20 <smithl20@mail.wlu.edu>
2019-10-29 11:59:27 -0400

    completed filtering for Percent of adults aged 18 years and older who have obesity

--- dataManipulation.R
+++ dataManipulation.R
@@ -2,4 +2,4 @@ View(obesityData)
 library(tidyverse)
 library(dplyr)
 
-obesityGeneral <- filter(obesityData, Question == "Percent of adults age 18 years and older who have obesity")
+obesityGeneral <- filter(obesityData, Question == "Percent of adults aged 18 years and older who have obesity")
