9460e5ae68bf561bccc2d909578f5ce2aa5606e6
Katherine Degner <degnerk20@mail.wlu.edu>
2019-10-31 13:14:39 -0400

    changing state into factor

--- obesityIncome.r
+++ obesityIncome.r
@@ -11,21 +11,26 @@ obesityIncome2018 <- obesityGeneral2018 %>% select(YearStart, LocationAbbr, Ques
 
 #removing all data that have NA as value for Income
 
-obesityIncomeData <- obesityIncome2018 %>% 
+obesityIncome <- obesityIncome2018 %>% 
   filter(Income != "N/A")
 
 #changing variable names
 
-colnames(obesityIncomeData) [1] <- "year"
-colnames(obesityIncomeData) [2] <- "location"
-colnames(obesityIncomeData) [3] <- "question"
-colnames(obesityIncomeData) [4] <- "percent"
-colnames(obesityIncomeData) [5] <- "sampleSize"
-colnames(obesityIncomeData) [6] <- "incomeLevel"
+colnames(obesityIncome) [1] <- "year"
+colnames(obesityIncome) [2] <- "location"
+colnames(obesityIncome) [3] <- "question"
+colnames(obesityIncome) [4] <- "percent"
+colnames(obesityIncome) [5] <- "sampleSize"
+colnames(obesityIncome) [6] <- "incomeLevel"
 
 #calculations
-obesityIncomeData <- obesityIncomeData %>%
+obesityIncome <- obesityIncomeData %>%
   as_tibble()                                %>% 
   mutate(
     numberObese = sampleSize*(percent/100))
 
+#turning states into factors
+
+obesityIncomeData$state <- as.factor(obesityIncomeData$state)
+
+summary(obesityIncomeData)
