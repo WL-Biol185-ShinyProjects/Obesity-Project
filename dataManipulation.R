d0f54a6e63ce774b66c7172722802378fb34b6e7
smithl20 <smithl20@mail.wlu.edu>
2019-10-31 12:50:07 -0400

    Filtered obesity dataset to create heat map. I filtered it for 2017 and 2018 and also for only the columns that I wanted for heat map.

--- dataManipulation.R
+++ dataManipulation.R
@@ -4,7 +4,6 @@ library(dplyr)
 
 obesityGeneral <- filter(obesityData, Question == "Percent of adults aged 18 years and older who have obesity")
 
-obesityGeneral2018 <- filter(obesityGeneral, YearStart == "2018")
+obesityGeneralYear <- filter(obesityGeneral, YearStart == c("2018", "2017"))
 
 
-#for obesity heatmap
---
+++ obesityHeatMap.r
@@ -0,0 +1,23 @@
+View(obesityData)
+library(tidyverse)
+library(dplyr)
+
+#filtering for specific question
+obesityGeneral <- filter(obesityData, Question == "Percent of adults aged 18 years and older who have obesity")
+
+#filtering for 2017 and 2018
+obesityGeneralYear <- filter(obesityGeneral, YearStart == c("2018", "2017"))
+
+#filter table for 5 columns that we want
+obesityHeat <- obesityGeneralYear %>%
+  select(1, 3, 8, 11, 17)
+ 
+#renamed columns
+colnames (obesityHeat) [1]  <- "year"
+colnames (obesityHeat) [2]  <- "state"
+colnames (obesityHeat) [3]  <-  "question" 
+colnames (obesityHeat) [4]  <- "percent" 
+colnames (obesityHeat) [5]  <- "sampleSize"
+
+obesityHeatPercent <- obesityHeat %>% #taking of N/A from percent column 
+  filter(percent != "N/A")
