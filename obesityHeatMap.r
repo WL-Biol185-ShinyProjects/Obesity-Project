be5420740731b9e984b7dbdb8a393e336bd33ecc
smithl20 <smithl20@mail.wlu.edu>
2019-11-14 13:19:56 -0500

    changed the check boxes for the year and also added selection options for the check boxes and states

--- obesityHeatMap.r
+++ obesityHeatMap.r
@@ -1,12 +1,14 @@
 obesityHeatTab <- list(
   
-  titlePanel("Obesity in the United States"   ),
+  titlePanel("Obesity in the United States"),
   
   fluidRow(box(width = 12, background = "black", p("This is a map of the United States detailing obesity in 2018"))),
   
   fluidRow(leafletOutput("myHeatMap")),
   
   fluidRow(box(width = 12, background = "black", p("The plot below shows an overview of obesity in the United States from 2009-2018"))),
+  
+  sidebarLayout(
   
   sidebarPanel(
   checkboxGroupInput("yearInput", "Choose the Year:",
@@ -18,9 +20,10 @@ obesityHeatTab <- list(
         "2015"  = "2015",
         "2016"  = "2016",
         "2017"  = "2017",
-        "2018"  = "2018")
-    ),
-    textOutput("checkYear"),
+        "2018"  = "2018"),
+        selected = list("2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018"),
+        inline = TRUE),
+
     
     selectInput(inputId = "state",
                 label = "Choose States:",
@@ -31,15 +34,24 @@ obesityHeatTab <- list(
                   "NM", "NV", "NY", "OH", "OK", "OR", "PA", "PR", "RI", "SC", "SD",
                   "TN", "TX", "US", "UT", "VA", "VI", "VT", "WA", "WI", "WV", "WY"
                 ),
+                selected = list(
+                  "AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA",
+                  "GU", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD",
+                  "ME", "MI", "MN", "WA", "WI", "WV", "WY"
+                ),
                 selectize = TRUE,
                 multiple  = TRUE
                 
     ),
-    textOutput("stateResult2"),
-    mainPanel(plotOutput("myLineGraph"), width = "100%")
-    )
-    )
-
+    textOutput("stateResult2") 
+    ),
+    mainPanel(plotOutput("myLineGraph"))
+  )
+  ) 
+    
+    
+    
+ 
           
 
 
@@ -85,6 +97,66 @@ obeseTotal$yearNum <- as.numeric(as.character(obeseTotal$year))
 
 
 ggplot(obeseTotal, aes(yearNum, percentObese, color=state)) + geom_line()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
