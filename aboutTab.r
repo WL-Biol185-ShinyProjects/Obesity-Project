a8bd38174d6bb1a2ee43235a0da68e7d5e70f198
smithl20 <smithl20@mail.wlu.edu>
2019-12-09 21:54:28 -0500

    added '20 to Laney Smith's name

--- aboutTab.r
+++ aboutTab.r
@@ -3,7 +3,7 @@ aboutTab <- list(
   h1(strong("About the Creators"), align = "center", style = "color:#990000"),
   br(),
   
-  h3("The creators of this shiny application consist of Kyle Brophy ('20), Katherine Degner ('20), and Laney Smith (20).  All three of us are Neuroscience majors at W&L and enjoy analyzing factors that contribute to health phenomena in America.", align = "center"),
+  h3("The creators of this shiny application consist of Kyle Brophy ('20), Katherine Degner ('20), and Laney Smith ('20).  All three of us are Neuroscience majors at W&L and enjoy analyzing factors that contribute to health phenomena in America.", align = "center"),
   br(),
   
   h3("We hope you find this website useful to identify correlations between education & income level with obesity throughout the United States.", align = "center"),
